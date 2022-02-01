54f5efb83b26c3549397592e766de9d828851d40
Ben Lirio <benjlirio@gmail.com>
2022-02-01 14:52:10 +0000

    OCaml Config

--- vim/init.py
+++ vim/init.py
@@ -1,6 +1,8 @@
 import vim
 
 vim.command("command Math :py3file ~/src/github.com/BenLirio/config/vim/eval-line.py")
+vim.command("command OcamlComplete :py3file ~/src/github.com/BenLirio/config/vim/ocaml-complete.py")
+vim.command("command SchemeComplete :py3file ~/src/github.com/BenLirio/config/vim/scheme-complete.py")
 vim.command("nm M :py3file ~/src/github.com/BenLirio/config/vim/manual.py<CR>")
 vim.command("nm 1M :py3file ~/src/github.com/BenLirio/config/vim/manual-edit.py<CR>")
 vim.command("colorscheme torte")
@@ -10,6 +12,6 @@ vim.command("syntax on")
 vim.command("set autoindent")
 
 vim.command("filetype plugin indent on")
-vim.command("set tabstop=4")
-vim.command("set shiftwidth=4")
+vim.command("set tabstop=2")
+vim.command("set shiftwidth=2")
 vim.command("set expandtab")
